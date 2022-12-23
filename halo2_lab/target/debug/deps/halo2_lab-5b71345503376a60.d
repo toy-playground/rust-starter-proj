a89d29452547b32702f9273825d13c0d0955675e
Wang Zuobin <wangzuobin@hyperchain.cn>
2022-12-23 13:06:32 +0800

    init halo2 sub-repo

---
+++ halo2_lab/target/debug/deps/halo2_lab-5b71345503376a60.d
@@ -0,0 +1,5 @@
+/Users/wangzuobin/projects/rust-starter-proj/halo2_lab/target/debug/deps/halo2_lab-5b71345503376a60.rmeta: src/main.rs
+
+/Users/wangzuobin/projects/rust-starter-proj/halo2_lab/target/debug/deps/halo2_lab-5b71345503376a60.d: src/main.rs
+
+src/main.rs:
