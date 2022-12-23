5a8fbdc127a7e7502718c9e0fd19152a607c42d0
dreamerryao <zuobinwang3@gmail.com>
2022-12-23 13:07:30 +0800

    fix(halo2_lab): remove target

--- halo2_lab/target/debug/deps/halo2_lab-1020f01c8f830544.d
+++
@@ -1,5 +0,0 @@
-/Users/wangzuobin/projects/rust-starter-proj/halo2_lab/target/debug/deps/halo2_lab-1020f01c8f830544.rmeta: src/main.rs
-
-/Users/wangzuobin/projects/rust-starter-proj/halo2_lab/target/debug/deps/halo2_lab-1020f01c8f830544.d: src/main.rs
-
-src/main.rs:
--- halo2_lab/target/debug/deps/halo2_lab-5b71345503376a60.d
+++
@@ -1,5 +0,0 @@
-/Users/wangzuobin/projects/rust-starter-proj/halo2_lab/target/debug/deps/halo2_lab-5b71345503376a60.rmeta: src/main.rs
-
-/Users/wangzuobin/projects/rust-starter-proj/halo2_lab/target/debug/deps/halo2_lab-5b71345503376a60.d: src/main.rs
-
-src/main.rs:
